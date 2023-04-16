03083d0fb71c2aceb34028dd1712714febb72c53
Volodymyr Lalykin <jazzman1403@gmail.com>
2023-04-16 21:08:10 +0300

    Remove a redundant text from the translation

--- src/test/java/pro/jazzman/odmiana/services/wikislownik/HtmlTest.java
+++ src/test/java/pro/jazzman/odmiana/services/wikislownik/HtmlTest.java
@@ -1,7 +1,6 @@
 package pro.jazzman.odmiana.services.wikislownik;
 
 import org.jsoup.nodes.Document;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +10,6 @@ import pro.jazzman.odmiana.exceptions.ApplicationRuntimeException;
 import pro.jazzman.odmiana.parsers.Parser;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -29,5 +27,27 @@ class HtmlTest {
         assertThatThrownBy(
             () -> new Html("<html></html>", parser).parse(LANG)
         ).isExactlyInstanceOf(ApplicationRuntimeException.class);
+    }
+
+    @Test
+    void translation() {
+        String markup = """
+            <ul>
+                <li>ukraiński:
+                    <span>(1.1)</span>
+                    <a href="/">test</a>
+                </li>
+                <li>
+                    angielski:
+                    <span>(1.1)</span> <a href="/">foo</a> <link rel="" href=""><span><a href="/"><span><span>m</span></span></a></span> (qux);
+                    <span>(1.2)</span> <a href="/">bar</a> <link rel="" href=""><span><a href="/"><span><span>ż</span></span></a></span> (quux),
+                    <span>(1.3)</span> <a href="/">baz</a> <link rel="" href=""><span><a href="/"><span><span>n</span></span></a></span> (quuux);
+                    <span>(1.4)</span> <a href="/">foo</a> <link rel="" href=""><span><a href="/"><span><span>n</span></span></a></span> (quuuux),
+                    <span>(1.5)</span> <a href="/">navy blue</a> <link rel="" href=""><span><a href="/"><span><span>n</span></span></a></span> (blue)
+                </li>
+            </ul>
+            """;
+
+        assertThat(new Html(markup, parser).translation("en")).isEqualTo("foo, bar, baz, navy blue");
     }
 }
